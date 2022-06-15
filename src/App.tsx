97488b940ae202db3ed1ca2c22040c44934745da
bh_jang <operqudgns10@gmail.com>
2022-06-16 03:00:32 +0900

    add: styled-components 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+import Button from './components/Buttons';
 
 const App =  () => {
-    return <div>App.tsx</div>
+    return (
+        <AppContainer>
+            <Button>Styled Component Button</Button>
+        </AppContainer>
+    )
 }
+
+const AppContainer = styled.div`
+    width: 100%;
+    height: 100%;
+`
 
 export default App;
---
+++ src/components/Buttons.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+    background-color: black;
+    height: 40px;
+    color: white;
+`
+
+export default Button;
